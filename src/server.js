c45dcb772302f60131d2d92b4f1f50a792361ec2
dinadahliana <dina.dahliana@students.itb.ac.id>
2023-02-14 20:26:56 +0700

    create server and routes module

--- src/server.js
+++ src/server.js
@@ -1,1 +1,14 @@
-console.log('Hawo');
+const Hapi = require('@hapi/hapi');
+
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 9000,
+    host: 'localhost',
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
