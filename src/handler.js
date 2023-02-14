a37a2c8f3d994d6410675d5d0a9924cd5dc0e348
dinadahliana <dina.dahliana@students.itb.ac.id>
2023-02-14 21:04:49 +0700

    create getBookByIdHandler

--- src/handler.js
+++ src/handler.js
@@ -80,8 +80,31 @@ const getAllBooksHandler = () => ({
   },
 });
 
+const getBookById = (request, h) => {
+  const {id} = request.params;
+
+  const book = books.filter((b) => b.id === id)[0];
+
+  if (book !== undefined) {
+    return {
+      status: 'succes',
+      data: {
+        book,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
 
 module.exports = {
   addBooksHandler,
   getAllBooksHandler,
+  getBookById,
 };
